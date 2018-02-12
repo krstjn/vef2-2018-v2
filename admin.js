73a59e161f9e685f6a785cb99cc06eac389c7397
KristjanP <kth130@hi.is>
2018-02-12 14:54:03 +0000

    psql tenging sett

--- admin.js
+++ admin.js
@@ -3,5 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 /* todo */
+const { Client } = require('pg');
 
+const connectionString = 'postgres://notandi:123@localhost/v2';
+
+const client = new Client({ connectionString });
+client.connect();
+
+async function select() {
+  try {
+    const res = await client.query('SELECT * FROM info');
+    console.log(res.rows);
+  } catch (e) {
+    console.error('Error selecting', e);
+  }
+  await client.end();
+}
+
+select().catch(e => console.error(e));
 module.exports = router;
