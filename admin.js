01e44407605766fb33c5b0c76f3a02c4024d718f
KristjanP <kth130@hi.is>
2018-02-14 03:10:14 +0000

    virkni fyrir innskráningu

--- admin.js
+++ admin.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const users = require('./users');
+const passport = require('passport');
+const { Strategy } = require('passport-local');
 
 const router = express.Router();
 
@@ -7,18 +10,20 @@ const { Client } = require('pg');
 
 const connectionString = 'postgres://notandi:123@localhost/v2';
 
-const client = new Client({ connectionString });
-client.connect();
 
 async function select() {
-  try {
-    const res = await client.query('SELECT * FROM info');
-    console.log(res.rows);
-  } catch (e) {
-    console.error('Error selecting', e);
-  }
+  const query = 'SELECT * FROM info';
+
+  const client = new Client({ connectionString });
+  await client.connect();
+  const results = await client.query(query);
   await client.end();
+
+  return results.rows;
 }
 
-select().catch(e => console.error(e));
+
+// select().catch(e => console.error(e));
+
+
 module.exports = router;
