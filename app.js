e332116e5bfcae0d02db1f3aa760c93a0fa7299f
KristjanP <kth130@hi.is>
2018-02-14 03:10:54 +0000

    virkni fyrir innskráningu

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('passport');
+const { Strategy } = require('passport-local');
 
 const form = require('./form');
 const admin = require('./admin');
+const users = require('./users');
 
 const app = express();
 
@@ -13,8 +16,73 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const sessionSecret = 'leyndarmál';
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+function strat(username, password, done) {
+  users
+    .findByUsername(username)
+    .then((user) => {
+      if (!user) {
+        return done(null, false);
+      }
+
+      return users.comparePasswords(password, user);
+    })
+    .then(res => done(null, res))
+    .catch((err) => {
+      done(err);
+    });
+}
+
+passport.use(new Strategy(strat));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  users
+    .findById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err));
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', form);
 app.use('/admin', admin);
+
+app.use((req, res, next) => {
+  if (req.isAuthenticated()) {
+    // getum núna notað user í viewum
+    res.locals.user = req.user;
+  }
+
+  next();
+});
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
+  }),
+  (req, res) => {
+    res.redirect('/admin');
+  },
+);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 function notFoundHandler(req, res, next) { // eslint-disable-line
   res.status(404).render('error', { title: '404' });
@@ -25,8 +93,9 @@ function errorHandler(err, req, res, next) { // eslint-disable-line
   res.status(500).render('error', { err });
 }
 
+
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
