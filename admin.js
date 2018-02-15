190e12d59c23bf084fca903494466325f2acdfa0
KristjanP <kth130@hi.is>
2018-02-15 14:21:45 +0000

    login virkni færð yfir í admin úr form

--- admin.js
+++ admin.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const users = require('./users');
-const passport = require('passport');
-const { Strategy } = require('passport-local');
+const json2csv = require('json2csv');
 
 const router = express.Router();
 
-/* todo */
 const { Client } = require('pg');
 
 const connectionString = 'postgres://notandi:123@localhost/v2';
 
-
-async function select() {
+async function getData() {
   const query = 'SELECT * FROM info';
 
   const client = new Client({ connectionString });
@@ -22,8 +18,29 @@ async function select() {
   return results.rows;
 }
 
+function ensureLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
-// select().catch(e => console.error(e));
+  return res.redirect('/login');
+}
 
+router.get('/download', ensureLoggedIn, async (req, res) => {
+  const filename = 'test.csv';
+  const data = await getData();
+  const fields = ['dagsetning', 'nafn', 'netfang', 'fjoldi', 'ssn'];
+  const csv = json2csv({ data, fields, del: ';' });
+  res.set(
+    'Content-Disposition',
+    `attachment; filename="${filename}"`,
+  );
+  res.send(csv);
+});
+
+router.get('/', ensureLoggedIn, async (req, res) => {
+  const data = await getData();
+  res.render('admin', { data });
+});
 
 module.exports = router;
