38cc05104c47efafa6cfadf390e618047626aeb5
KristjanP <kth130@hi.is>
2018-02-15 14:22:03 +0000

    xss

--- form.js
+++ form.js
@@ -27,49 +27,55 @@ async function insertData(data) {
   await client.end();
 }
 
-async function getData() {
-  const query = 'SELECT * FROM info';
-
-  const client = new Client({ connectionString });
-  await client.connect();
-  const results = await client.query(query);
-  await client.end();
-
-  return results.rows;
-}
-
 router.post('/', async (req, res) => {
-  const data = req.body;
+  const {
+    name = '',
+    email = '',
+    ssn = '',
+    count = 0,
+  } = req.body;
+
   const errors = [];
 
-  // Þetta er bara validation! Ekki sanitization
-  if (data.name === '') {
+  if (name === '') {
     errors.push('Nafn má ekki vera tómt');
   }
 
-  if (data.email === '' || data.email.indexOf('@') < 0) {
+  if (email === '') {
     errors.push('Netfang má ekki vera tómt');
+  }
+
+  if (email.indexOf('@') < 0) {
     errors.push('Netfang verður að vera netfang');
-    
   }
 
-  if (data.ssn === '' || !/^[0-9]{6}-?[0-9]{4}$/.test(data.ssn)) {
+  if (ssn === '') {
     errors.push('Kennitala má ekki vera tóm');
-    errors.push('Verður að vera kennitala');
-    
   }
 
-  if (data.count === '' || !/^[1-9][0-9]*$/) {
+  if (!/^[0-9]{6}-?[0-9]{4}$/.test(ssn)) {
+    errors.push('Verður að vera kennitala');
+  }
+
+  if (count === '' || !/^[1-9][0-9]*$/) {
     errors.push('Þarf að vera heiltala stærri en 0');
   }
+  console.log(errors);
   const loggedIn = req.isAuthenticated();
 
   if (errors.length > 0) {
     return res.render('form', {
       errors,
-      data,
+      data: req.body,
+      loggedIn,
     });
   }
+  const data = {
+    name: xss(name),
+    email: xss(email),
+    ssn: xss(ssn),
+    count: xss(count),
+  };
 
   await insertData(data);
 
@@ -86,20 +92,6 @@ router.get('/thanks', (req, res) => {
 
 router.get('/login', (req, res) => {
   res.render('login');
-});
-
-function ensureLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-  console.log('ensure logged in');
-    
-    return next();
-  }
-
-  return res.redirect('/login');
-}
-router.get('/admin', ensureLoggedIn, async (req, res) => {
-  const data = await getData();
-  res.render('admin', { data });
 });
 
 router.get('/', form);
